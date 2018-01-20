2e51adaad5a1c6758df7fc6db1a1ff58548b80d6
Arbel Israeli <arbel03@gmail.com>
2018-01-20 16:24:23 +0200

    Removed the use of Atomic operations.

--- src/memory/heap.rs
+++ src/memory/heap.rs
@@ -1,34 +1,30 @@
 use alloc::heap::{ Alloc, AllocErr, Layout };
-use core::sync::atomic::{ AtomicUsize, Ordering };
 
 // TODO: improve allocator
 pub struct BumpAllocator {
     end: usize,
-    current: AtomicUsize,
+    current: usize,
 }
 
 impl BumpAllocator {
     pub const fn new(start: usize, size: usize) -> Self {
-        BumpAllocator { end: start+size, current:AtomicUsize::new(start) }
+        BumpAllocator { end: start + size, current: start }
     }
 }
 
 unsafe impl <'a> Alloc for &'a BumpAllocator {
     unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
-        loop {
-            // println!("Reqeusting layout: {:?}", layout);
-            let current = self.current.load(Ordering::Relaxed);
-            let alloc_start = align_up(current, layout.align());
-            let alloc_end = alloc_start + layout.size();
-
-            if alloc_end <= self.end {
-                let new_current = self.current.compare_and_swap(current, alloc_end, Ordering::Relaxed);
-                if new_current == current {
-                    return Ok(alloc_start as *mut u8);
-                }
-            } else {
-                return Err(AllocErr::Exhausted{ request: layout });
-            }
+        // println!("Reqeusting layout: {:?}", layout);
+        let current = self.current;
+        let alloc_start = align_up(current, layout.align());
+        let alloc_end = alloc_start + layout.size();
+        
+        if alloc_end <= self.end {
+            //println!("Allocating at {:#x}", alloc_start);
+            return Ok(alloc_start as *mut u8);
+        } else {
+            print!("Allocator exhausted.");
+            return Err(AllocErr::Exhausted{ request: layout });
         }
     }
 
